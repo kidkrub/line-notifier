0a7b1ce9b3bcbae80c6d7ebc734059130df82cd6
kritsada <devsecops@chularat.com>
2023-04-17 18:41:16 +0700

    update example

--- example/index.js
+++ example/index.js
@@ -1,14 +1,26 @@
 const LineNotifier = require("../index");
-const sender = new LineNotifier(process.env.ACCESS_TOKEN);
+const sender = new LineNotifier("client_id", "client_secret", "redirect_uri");
 
-sender.send({ message: "Only message" });
-sender.send({ message: "sticker", stickerPackageId: 446, stickerId: 1992 });
-sender.send({ imageFile: "./3135715.png", message: "imageFile" });
-sender.send({
+const url = sender.authorize("test-state");
+
+sender.send("AccessToken", { message: "Only message" });
+sender.send("AccessToken", {
+  message: "sticker",
+  stickerPackageId: 446,
+  stickerId: 1992,
+});
+sender.send("AccessToken", {
+  imageFile: "./3135715.png",
+  message: "imageFile",
+});
+sender.send("AccessToken", {
   message: "image URL",
   imageThumbnail: "https://i.ytimg.com/vi/fOd16PT1S7A/maxresdefault.jpg",
   imageFullsize: "https://i.ytimg.com/vi/fOd16PT1S7A/maxresdefault.jpg",
 });
-sender.send({ message: "Disabled Notification", notificationDisabled: true });
-sender.status();
-sender.revoke().then((res) => console.log(res));
+sender.send("AccessToken", {
+  message: "Disabled Notification",
+  notificationDisabled: true,
+});
+sender.status("AccessToken");
+sender.revoke("AccessToken").then((res) => console.log(res));
